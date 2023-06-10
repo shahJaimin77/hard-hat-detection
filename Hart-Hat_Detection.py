7b0db294c8b2576089d728cf942021297ce9816a
Sneha Thakur <85390398+ThakurSneha@users.noreply.github.com>
2023-06-10 21:29:57 +0530

    Python File for Hard Hat Detection

---
+++ Hart-Hat_Detection.py
@@ -0,0 +1,38 @@
+import torch
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+
+model = torch.hub.load('ultralytics/yolov5', 'custom',
+                       path='best_hh.pt', force_reload=True)
+
+
+# Open the video file
+cap = cv2.VideoCapture('D:/Sneha/Intership of Arunoday Tech/HardHat_Detection/testing/video4.mp4')
+
+# Check if the file was opened successfully
+if not cap.isOpened():
+    print("Error opening video file")
+
+# Loop over the frames in the video file
+while cap.isOpened():
+    # Read a frame from the video file
+    ret, frame = cap.read()
+
+    # Check if the frame was read successfully
+    if not ret:
+        break
+
+    # Show the frame in a window
+
+    results = model(frame)
+
+    cv2.imshow('Fire Detection', np.squeeze(results.render()))
+
+    # Wait for 25 milliseconds or until the user presses a key
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        break
+
+# Release the video file and close all windows
+cap.release()
+cv2.destroyAllWindows()
